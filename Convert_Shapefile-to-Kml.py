6ea61478659a27331ebdbc5ff33789cddfc83faf
Abraão Nascimento <abraaoalvesdonascimento@gmail.com>
2016-03-07 16:54:18 -0300

    Create Convert_Shapefile-to-Kml.py

---
+++ Convert_Shapefile-to-Kml.py
@@ -0,0 +1,36 @@
+#
+#------------------CONVERT SHAPEFILE TO KML -------------------
+import os
+
+# Setting workspace
+workspace = "C:\\data\\shapefile"
+
+# Setting out path file
+out_file = ""
+
+# Setting Coordinate Reference System WGS84
+reference_system = QgsCoordinateReferenceSystem(4326)
+
+# Try convert shapefile to kml
+try:
+
+    # for each shapefile in workspace
+    for row in os.listdir(workspace):
+
+        # Find the geometry file 
+        if row[-4:] == '.shp':
+
+            # Get the name file
+            markup = row.find(".")
+            kml_name = row[:markup] + ".kml"
+            
+            # Create a new temporary layer
+            vector_lyr = QgsVectorLayer(workspace + "\\" + row, row[:markup], "ogr")
+
+            print "Converting Shapefile to Kml"
+            # Convert the layer to kml file
+            QgsVectorFileWriter.writeAsVectorFormat(vector_lyr, out_file + '\\' + kml_name, "utf-8", reference_system, "KML")
+
+            print "Success!"
+except:
+    print "It was not possible to convert shapefile to kml"
