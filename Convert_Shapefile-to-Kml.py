ce460c33fe9e05be749a895a75e0215742c8303a
Abraão Nascimento <abraaoalvesdonascimento@gmail.com>
2016-03-21 16:12:08 -0300

    Update Convert_Shapefile-to-Kml.py

--- Convert_Shapefile-to-Kml.py
+++ Convert_Shapefile-to-Kml.py
@@ -4,7 +4,7 @@ import os
 from qgis.core import QgsCoordinateReferenceSystem, QgsVectorLayer
 
 # Setting workspace
-workspace = "C:\\data\\shapefile"
+workspace = "C:\\data"
 
 # Setting out path file
 out_file = "C:\\data\\out"
@@ -14,24 +14,24 @@ reference_system = QgsCoordinateReferenceSystem(4326)
 
 # Try convert shapefile to kml
 try:
+        # For each shapefile in workspace
+for row in os.listdir(workspace):
 
-    # For each shapefile in workspace
-    for row in os.listdir(workspace):
+    # Find the geometry file
+    if row[-4:] == '.shp':
+        # Get the name file
+        markup = row.find(".")
+        kml_name = row[:markup] + ".kml"
 
-        # Find the geometry file 
-        if row[-4:] == '.shp':
+        # Create a new temporary layer
+        vector_lyr = QgsVectorLayer(workspace + "\\" + row, row[:markup], "ogr")
+        vector_lyr .setProviderEncoding(u'latin1')
+        vector_lyr .dataProvider().setEncoding(u'latin1')
 
-            # Get the name file
-            markup = row.find(".")
-            kml_name = row[:markup] + ".kml"
-            
-            # Create a new temporary layer
-            vector_lyr = QgsVectorLayer(workspace + "\\" + row, row[:markup], "ogr")
+        # Convert the layer to kml file
+        print "Converting Shapefile to Kml"
+        QgsVectorFileWriter.writeAsVectorFormat(vector_lyr, out_file + '\\' + kml_name, "latin1", reference_system, "KML")
 
-            # Convert the layer to kml file
-            print "Converting Shapefile to Kml"
-            QgsVectorFileWriter.writeAsVectorFormat(vector_lyr, out_file + '\\' + kml_name, "utf-8", reference_system, "KML")
-            
-            print "Success!"
+        print "Success!"
 except:
     print "It was not possible to convert shapefile to kml"
