5cd09d0fdb495a9e786190ec761e2cca0d006072
Abraão Nascimento <abraaoalvesdonascimento@gmail.com>
2016-04-11 17:27:11 -0300

    Convert shp multipart to shp singleparts

---
+++ Convert_Multipart-to-Singleparts.py
@@ -0,0 +1,36 @@
+#========================Multipart to singleparts===============================
+""""
+Processs to save and to run in conslose python from QGIS
+"""
+
+import os
+import processing
+
+# Setting the process to run in QGIS
+processing.alglist("Multipart to singleparts")
+
+# Setting the path for input and out put shapefile
+workspace = "C:\\Users\\abraao.nascimento\\Downloads\\2016_04\\carga_shp"
+
+try:
+    # For each shapefile in workspace
+
+    for row in os.listdir(workspace):
+
+        # Find the geometry file
+        if row[-4:] == '.shp':
+
+            # Get the path of shapefile
+            filePath = str(workspace) + "\\" + row
+
+	    # Get the name new file
+            markup = row.find(".")
+            newFileName = row[:markup] + "_singlePart.shp"
+
+            # Convert the layer to kml file
+            print "Converting multipart to singleparts"
+            processing.runalg("qgis:multiparttosingleparts",filePath,newFileName)
+
+            print "Success!"
+except:
+    print "It was not possible to convert multipart to singleparts"
